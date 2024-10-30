cec3f89fb89683d3b183ca0f639e1d528cbb5684
alex <alexander.pelle@edu.itspiemonte.it>
2024-10-30 10:08:44 +0100

    Creazione progetto spring

---
+++ GuessPlayer/src/main/java/com/quiz/GuessPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessPlayerApplication.class, args);
+	}
+
+}
