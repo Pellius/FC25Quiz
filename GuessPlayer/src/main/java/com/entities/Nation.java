31e1b854dc927e946708a49d3faa77601b4323cd
alex <alexander.pelle@edu.itspiemonte.it>
2024-10-30 14:55:06 +0100

    Entità PlayerPosition creata

--- GuessPlayer/src/main/java/com/entities/Nation.java
+++ GuessPlayer/src/main/java/com/entities/Nation.java
@@ -9,5 +9,21 @@ public class Nation    //Entità Nation
 {
    private int id_nation; //Primary key della tabella Nation
    
-   private String nation_name; 
+   private String nation_name;
+
+public int getId_nation() {
+	return id_nation;
+}
+
+public void setId_nation(int id_nation) {
+	this.id_nation = id_nation;
+}
+
+public String getNation_name() {
+	return nation_name;
+}
+
+public void setNation_name(String nation_name) {
+	this.nation_name = nation_name;
+} 
 }
---
+++ GuessPlayer/src/main/java/com/entities/PlayerPosition.java
@@ -0,0 +1,42 @@
+package com.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Player_Position")
+public class PlayerPosition 
+{
+	@JoinColumn(name = "id_player")
+	private int player;
+	
+	@JoinColumn(name = "id_position")
+	private int position;
+    
+	private boolean is_main;
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public boolean isIs_main() {
+		return is_main;
+	}
+
+	public void setIs_main(boolean is_main) {
+		this.is_main = is_main;
+	}
+}
