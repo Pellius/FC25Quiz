bb4d34470ec641e3a0ffbd037d02311c5c4b7405
alex <alexander.pelle@edu.itspiemonte.it>
2024-10-30 11:21:46 +0100

    Entità Player creata

---
+++ GuessPlayer/src/main/java/com/entities/Player.java
@@ -0,0 +1,37 @@
+package com.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Player") 
+public class Player    //Entità Player
+{
+
+   private int id_player; //Primary key della tabella Player
+   
+   private String player_name;
+   
+   private int overall;
+   
+   private String position;
+   
+   private int weak_foot;
+   
+   private int skill_moves;
+   
+   private int height;
+   
+   private int age;
+   
+   private String img;
+   
+   @JoinColumn(name = "Nation") //Foreign key collegata alla primary key della tabella Nation
+   private int id_nation;      
+   
+   @JoinColumn(name = "Team")  //Foreign key collegata alla primary key della tabella Team
+   private int id_team; 
+   
+}
